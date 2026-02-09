8636ee85c0b7b0a1d84fb46b74284645a9baed52
Nordine Benidiri <nordinebenidiri@gmail.com>
2026-02-09 19:57:12 +0100

    updates ...

--- src/modules/dashboard/dtos/index.ts
+++ src/modules/dashboard/dtos/index.ts
@@ -1,3 +1,5 @@
 export * from './dashboard-stats.dto';
 export * from './client-dashboard-stats.dto';
 export * from './client-dashboard-charts.dto';
+export * from './agent-dashboard-stats.dto';
+export * from './agent-dashboard-charts.dto';
